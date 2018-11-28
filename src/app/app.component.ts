239508d5a1e30ac64956680328540b6fd7b010b5
unknown <asincole@gmail.com>
2018-11-28 22:05:44 +0100

    main page layout updated with styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,38 @@
-import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'asingit-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   year = new Date().getFullYear();
+  profile: any;
   isCollapsed = false;
   triggerTemplate = null;
   @ViewChild('trigger') customTrigger: TemplateRef<void>;
 
-  constructor() {
-
+  constructor(private auth: AuthService) {
+    auth.handleAuthentication();
   }
+
+  ngOnInit() {
+    if (this.auth.userProfile) {
+      this.profile = this.auth.userProfile;
+    } else {
+      this.auth.getProfile((err, profile) => {
+        this.profile = profile;
+      });
+    }
+  }
+
   /** custom trigger can be TemplateRef **/
   changeTrigger(): void {
     this.triggerTemplate = this.customTrigger;
+  }
+
+  login(): void {
+    this.auth.login();
   }
 }
