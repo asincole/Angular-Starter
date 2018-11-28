0e7093781b1138ba0f7eb0f034caefaff14e0d94
unknown <asincole@gmail.com>
2018-11-28 22:04:29 +0100

    added auth service

---
+++ src/app/services/auth0-congif.ts
@@ -0,0 +1,11 @@
+export const AuthConfig: AuthConfig = {
+    clientID: 'clientIDValue',
+    domain: 'domainValue',
+    callbackURL: 'callbackURLValue'
+};
+
+interface AuthConfig {
+    clientID: string;
+    domain: string;
+    callbackURL: string;
+}
