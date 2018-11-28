7383ce876b9ee90b645a47bf0772d08ca4bd97b8
unknown <asincole@gmail.com>
2018-11-28 22:05:22 +0100

    added profile page

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'asingit-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+  profile: any;
+
+  constructor(private routeParams: ActivatedRoute,
+    private auth: AuthService) {
+  }
+
+  // ngOnInit() {
+  //   this.profile = this.routeParams.data;
+  //   console.log(this.routeParams);
+  // }
+
+  ngOnInit() {
+    if (this.auth.userProfile) {
+      this.profile = this.auth.userProfile;
+    } else {
+      this.auth.getProfile((err, profile) => {
+        this.profile = profile;
+      });
+    }
+  }
+
+}
---
+++ src/app/pages/profile/profile.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileComponent } from './profile.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+
+@NgModule({
+  declarations: [ProfileComponent],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    NgZorroAntdModule
+  ]
+})
+export class ProfileModule { }
